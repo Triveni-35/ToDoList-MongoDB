cfc0715613799f938774c4b39d01748aa45507cd
Pilla Triveni <ugs21016_cse.triveni@cbit.org.in>
2024-06-20 16:36:07 +0530

    aftr adding db commit

---
+++ todo-backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(cors());
+app.use(express.json());
+
+const todosRouter = require('./routes/todos');
+app.use('/todos', todosRouter);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
